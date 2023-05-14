b90b8d86374e24af41d2023a79e65c5fe80a9191
tracy macaulay-higgan <tracy@mad.geek.nz>
2023-05-14 21:16:59 +1200

    chor: problem 2 now working

--- src/App.js
+++ src/App.js
@@ -26,35 +26,41 @@ function App() {
     ));
   };
 
-  const renderBasketballTeam2 = (data) => {
-    return Object.entries(data).map(([key, value]) => (
-      <li key={key}>{`${key} ${value}`}</li>
-    ));
-  };
- 
+  const displayTeam = (data) => {
+    if (data) {
+      return Object.entries(data).map(([key, value]) => (
+        <li key={key}>{`${key} ${value}`}</li>
+      ));
+       } else {
+        return <li>No Team Provided</li>
+    }
+};
+
+
+
 const renderFilterFruits = (fruits) => {
-return Object.entries(fruits).map(([key, value]) => (
-  <li key={key}>{`${key} ${value}`}</li>
-));
+  return Object.entries(fruits).map(([key, value]) => (
+    <li key={key}>{`${key} ${value}`}</li>
+  ));
 
 }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        </header>
+return (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
 
 
     <h1>Problem 1 - Render Basketball Team</h1>
@@ -64,21 +70,21 @@ return Object.entries(fruits).map(([key, value]) => (
 
     <h1>Problem 2 - Refactor, On No Team Display something sane</h1>
     <ul>
-      {renderBasketballTeam2(basketballTeam)}
+      {displayTeam(basketballTeam)}
     </ul>
 
     <h1>Problem 3 - show fruits</h1>
     <ul>
       {renderFilterFruits(filterFruits)}
     </ul>
-      
-      
-    </div>
-    
-    
-  );
 
-  
+
+  </div>
+
+
+);
+
+
 }
 
 
